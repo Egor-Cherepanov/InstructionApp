d1a442eef6154282b429a7b5685ffdc1dd0a08f0
Egor-Cherepanov <105498881+Egor-Cherepanov@users.noreply.github.com>
2024-12-14 22:58:02 +0300

    InstructionApp

--- src/App.js
+++ src/App.js
@@ -1,57 +1,74 @@
-import styles from "./app.module.css"
-import data from "./data.json"
-import { useState } from "react"
+import styles from './app.module.css';
+import data from './data.json';
+import { useState } from 'react';
 
 const App = () => {
-  // Можно задать 2 состояния — steps и activeIndex
-  const [steps, setSeps] = useState(data)
-  const [activeIndex, setActiveIndex] = useState([])
-  console.log(steps)
-  const goBack = () => {}
-  const goForward = () => {}
-  const startOver = () => {}
+	// Можно задать 2 состояния — steps и activeIndex
+	const [steps, setSeps] = useState(data);
+	const [activeIndex, setActiveIndex] = useState(0);
 
-  // И определить 3 обработчика: Клик назад, Клик вперед, Начать сначала
+	const goBack = () => {
+		setActiveIndex(activeIndex - 1);
+	};
+	const goForward = () => {
+		setActiveIndex(activeIndex + 1);
+	};
+	const startOver = () => {
+		setActiveIndex(0);
+	};
 
-  // И 2 переменных-флага — находимся ли мы на первом шаге, и находимся ли на последнем
+	const stepsItem = (id, title, stylesInfo) => {
+		return (
+			<li className={styles['steps-item'] + ' ' + stylesInfo} key={id}>
+				<button
+					className={styles['steps-item-button']}
+					onClick={() => setActiveIndex(Number(id[2]) - 1)}
+				>
+					{id[2]}
+				</button>
+				{title}
+			</li>
+		);
+	};
 
-  return (
-    <div className={styles.container}>
-      <div className={styles.card}>
-        <h1>Инструкция по готовке пельменей</h1>
-        <div className={styles.steps}>
-          <div className={styles["steps-content"]}>
-            {/* Для получения активного контента использйте steps и activeIndex */}
-            Контент соответственный шагу. Сейчас активен шаг 3
-          </div>
-          <ul className={styles["steps-list"]}>
-            {steps.map(({ id, title, content }) => {
-              console.log(id)
-              return (
-                <li
-                  className={styles["steps-item"] + " " + styles.done}
-                  key={id}
-                >
-                  <button className={styles["steps-item-button"]}>
-                    {id[2]}
-                  </button>
-                  {title}
-                </li>
-              )
-            })}
-          </ul>
-          <div className={styles["buttons-container"]}>
-            <button className={styles.button}>Назад</button>
-            <button className={styles.button}>
-              Далее
-              {/* "Начать сначала", можно сделать этой же кнопкой, просто подменять обработчик и текст в зависимости от условия */}
-              {/* Или заменять всю кнопку в зависимости от условия */}
-            </button>
-          </div>
-        </div>
-      </div>
-    </div>
-  )
-}
+	return (
+		<div className={styles.container}>
+			<div className={styles.card}>
+				<h1>Инструкция по готовке пельменей</h1>
+				<div className={styles.steps}>
+					<div className={styles['steps-content']}>
+						{steps[activeIndex].content}
+					</div>
 
-export default App
+					<ul className={styles['steps-list']}>
+						{steps.map(({ id, title }, keyNum) => {
+							console.log(keyNum === activeIndex);
+							return activeIndex === keyNum
+								? stepsItem(id, title, styles.active)
+								: activeIndex > keyNum
+									? stepsItem(id, title, styles.done)
+									: stepsItem(id, title);
+						})}
+					</ul>
+					<div className={styles['buttons-container']}>
+						<button
+							className={styles.button}
+							onClick={goBack}
+							disabled={activeIndex === 0 ? true : false}
+						>
+							Назад
+						</button>
+						<button
+							className={styles.button}
+							onClick={activeIndex === 6 ? startOver : goForward}
+						>
+							{activeIndex === 6 ? 'Начать сначала' : 'Далее'}
+						</button>
+					</div>
+				</div>
+			</div>
+		</div>
+	);
+};
+
+export default App;
